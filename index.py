f83c63c728039ae6656e14de8b329fba6081d3db
WilliamFitzjohn <william_fitzjohn@icloud.com>
2023-11-26 09:34:01 -0500

    changed to use BASE_URL

--- index.py
+++ index.py
@@ -255,19 +255,19 @@ def returnApp():
     """
     This function is used to create the app and return it to waitress in the docker container
     """
-    # If DASH_BASE_URL is set, use DispatcherMiddleware to serve the app from that path
-    if 'DASH_BASE_URL' in os.environ:
+    # If BASE_URL is set, use DispatcherMiddleware to serve the app from that path
+    if 'BASE_URL' in os.environ:
         from flask import Flask
         from werkzeug.middleware.dispatcher import DispatcherMiddleware
         app.wsgi_app = DispatcherMiddleware(Flask('dummy_app'), {
-            os.environ['DASH_BASE_URL']: app.server
+            os.environ['BASE_URL']: app.server
         })
         # Added redirect to new path
         @app.wsgi_app.app.route('/')
         def redirect_to_dashboard():
             from flask import redirect
-            return redirect(os.environ['DASH_BASE_URL'])
+            return redirect(os.environ['BASE_URL'])
         return app.wsgi_app
 
-    # If no DASH_BASE_URL is set, just return the app server
+    # If no BASE_URL is set, just return the app server
     return app.server
